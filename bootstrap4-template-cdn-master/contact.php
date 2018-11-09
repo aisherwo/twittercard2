cf1ed1ba06700a72c23f6aec8f11f75d57be3756
aisherwo <43270225+aisherwo@users.noreply.github.com>
2018-11-09 09:53:16 -0500

    Add files via upload

---
+++ bootstrap4-template-cdn-master/contact.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+    crossorigin="anonymous"> <!-- Fontawesome -->
+  <!-- Fontawesome  -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+    crossorigin="anonymous">
+
+  <title>????Enter your title here, don't forget!!! </title>
+
+  
+
+  <style>
+    h3 {
+      color: green;
+    }
+  </style>
+</head>
+
+<body>
+
+   <?php include 'inc/navbar.php';?> 
+
+  <div class="jumbotron text-center">
+    <h1>Contact Us</h1>
+    <p>This is the page where you can contact us.</p>
+    <p><small>by Mr. M. - Confederation College</small></p>
+    <a class="btn btn-primary" href="#" role="button">Yoda's videos &raquo;</a>
+  </div>
+
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-4">
+        <h3>Column 1</h3>
+        <img class="img-fluid rounded-circle" src="https://assets.entrepreneur.com/content/3x2/2000/20170405204540-empire-yoda.jpeg?width=700&crop=2:1" alt="yoda">
+        <span style="font-size: 3em; color: Tomato;">
+          <i class="fas fa-stroopwafel"></i>
+        </span>
+        <i class="fas fa-rocket fa-xs"></i>
+        <i class="fas fa-rocket fa-sm"></i>
+        <i class="fas fa-rocket fa-lg"></i>
+        <i class="fas fa-rocket fa-2x"></i>
+        <p>Lorem ipsum dolor..</p>
+        <p>Ut enim ad..</p>
+      </div>
+
+      <div class="col-md-4">
+        <h3>Column 2</h3>
+        <p>Lorem ipsum dolor..</p>
+        <p>Ut enim ad..</p>
+      </div>
+
+      <div class="col-md-4">
+        <h3>Column 3</h3>
+        <p>Lorem ipsum dolor..</p>
+        <p>Ut enim ad..</p>
+      </div>
+
+    </div>
+  </div>
+
+  <hr>
+  <!-- link to html5 validator with image -->
+  <a href="https://validator.w3.org/nu/?showsource=yes&useragent=Validator.nu%2FLV+http%3A%2F%2Fvalidator.w3.org%2Fservices&acceptlanguage=&doc=https%3A%2F%2Fmrmccormack.github.io%2Fbootstrap4-template-cdn%2Findex.html">
+    <img src="https://www.w3.org/html/logo/downloads/HTML5_Badge_32.png" alt="valid html5">
+  </a>
+  <hr>
+
+<?php include 'inc/footer.php';?> 
+
+
+</body>
+
+</html>
---
+++ bootstrap4-template-cdn-master/inc/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Yoda Lives</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="aboutus.php">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Members only</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
